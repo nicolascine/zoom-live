53b9e03440fab9ff5b46a81865da7558de625774
Nicolás Silva <sovietfrance@gmail.com>
2020-11-04 02:18:26 -0300

    feat: add image overlay on session items

--- src/components/pages/Home/Item.tsx
+++ src/components/pages/Home/Item.tsx
@@ -10,7 +10,18 @@ const Item: React.FC<Session> = (props) => {
     <div className="col-md-4">
       <div className="card mb-4 shadow-sm">
         <Link to={`/session/${props.id}`}>
-          <LazyImage src={props.profile_img_url} alt={props.name} />
+          <div className="overlay-effect">
+            <LazyImage src={props.profile_img_url} alt={props.name} />
+            <div className="caption">
+              <span>
+                {props.description
+                  ? props.description.length > 190
+                    ? `${props.description.slice(0, 190)}...`
+                    : props.description
+                  : props.name}
+              </span>
+            </div>
+          </div>
         </Link>
         <div className="card-body">
           <p className="card-text">{props.name}</p>
