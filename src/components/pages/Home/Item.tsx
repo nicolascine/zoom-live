9df180fbcc0c88501c6e5bc1ed4bee47752ed761
Nicolás Silva <sovietfrance@gmail.com>
2020-11-05 22:11:18 -0300

    feat: update dateFormat and date-icon

--- src/components/pages/Home/Item.tsx
+++ src/components/pages/Home/Item.tsx
@@ -12,7 +12,7 @@ import { Session } from '../../../store/sessions/types';
 const Item: React.FC<Session> = (props) => {
   return (
     <div className="col-md-4">
-      <div className="card mb-4 shadow-sm">
+      <div className="card mb-4 shadow-sm" style={{ borderRadius: '10px' }}>
         <Link to={`/session/${props.id}`}>
           <div className="overlay-effect">
             {
@@ -40,7 +40,24 @@ const Item: React.FC<Session> = (props) => {
         <div className="card-body">
           <p className="card-text">{props.name}</p>
           <div className="d-flex justify-content-between align-items-center">
-            <small className="text-muted">
+            <small style={{ color: '#aaa' }}>
+              <svg
+                width="1.3em"
+                height="1.2em"
+                viewBox="0 0 18 20"
+                className="bi bi-clock"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"
+                />
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"
+                />
+              </svg>
               {unixTimestampToDate(
                 props.start_time,
                 config.DISPLAY_DATE_FORMAT
--- src/config/index.tsx
+++ src/config/index.tsx
@@ -8,7 +8,7 @@ const config = {
     date: Math.round(new Date().getTime() / 1000),
     limit: 100,
   },
-  DISPLAY_DATE_FORMAT: 'MM/DD/YYYY',
+  DISPLAY_DATE_FORMAT: 'LLL',
   VIDEO_IFRAME_URL: '//via.live/zoom_player/',
   NO_IMAGE_PLACEHOLDER: (text: string) =>
     `https://via.placeholder.com/320?text=${text.slice(0, 10)}...`,
